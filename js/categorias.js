44714447a10cf12200c964b538298ed9626a8544
Gabriel Baumgarten <gabrielbaumgarten@hotmail.com>
2021-08-11 03:58:57 -0300

    Listagem de produtos inacabada

--- js/categorias.js
+++ js/categorias.js
@@ -13,20 +13,26 @@ function ListarCategorias(){
 
         var divCategoria = document.createElement('div')
         divCategoria.classList.add('categoria')
+        divCategoria.setAttribute('id', categoria.id)
         var p = document.createElement('p')
         
-        p.innerHTML = categoria.nome
+        p.innerHTML = categoria.id + '   ' + categoria.nome
         
         var divButtons = document.createElement('div')
         divButtons.classList.add('buttons')
+        var link = document.createElement('a')
+        link.setAttribute('href','adm-alterar-categoria.html')
         var iconEdit = document.createElement('img')
         iconEdit.setAttribute('src','../assets/icon-edit.svg')
         iconEdit.setAttribute('alt','Editar')
+        iconEdit.setAttribute('onclick','PreencherId('+ categoria.id +')')
         var iconRemove = document.createElement('img')
         iconRemove.setAttribute('src','../assets/icon-remove.svg')
         iconRemove.setAttribute('alt','Remover')
+        iconRemove.setAttribute('onclick','RemoverCategoria('+ categoria.id +')')
 
-        divButtons.appendChild(iconEdit)
+        link.appendChild(iconEdit)
+        divButtons.appendChild(link)
         divButtons.appendChild(iconRemove)
 
         divCategoria.appendChild(p)
@@ -35,4 +41,78 @@ function ListarCategorias(){
         var divListarCategorias = document.getElementById('lista-categorias')
         divListarCategorias.appendChild(divCategoria)
     })
+}
+
+function CriarCategoria(){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    var nome_categoria = document.getElementById('nome_categoria').value
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', "http://loja.buiar.com/?key=3Tz81Yftd3C&c=categoria&t=inserir&nome="+ nome_categoria +"&f=json", false)
+    httpRequest.send()
+
+
+    if(resposta.status == 'OK'){
+        alert('Categoria criada com sucesso')
+    }
+}
+
+function RemoverCategoria(id){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', "http://loja.buiar.com/?key=3Tz81Yftd3C&c=produto&t=listar&f=json", false)
+    httpRequest.send()
+
+    var controle = true
+
+    resposta.dados.forEach( produto =>{
+        if(produto.categoria == id){
+            controle = false
+            return
+        }
+    })
+
+    if(controle){
+        httpRequest.open('GET', "http://loja.buiar.com/?key=3Tz81Yftd3C&c=categoria&t=remover&id="+ id +"&f=json", false)
+        httpRequest.send()
+
+        window.location.reload()
+    } else{
+        alert('Não foi possível remover a categoria, pois há produtos dentro dela!')
+    }
+}
+
+function AlterarCategoria(){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    var nome_categoria = document.getElementById('nome_categoria').value
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', "http://loja.buiar.com/?key=3Tz81Yftd3C&c=categoria&t=alterar&nome="+ nome_categoria +"&f=json", false)
+    httpRequest.send()
+
+
+    if(resposta.status == 'OK'){
+        alert('Categoria criada com sucesso')
+    }
+}
+
+function PreencherId(id){
+    var id_categoria = document.getElementById('id_categoria')
+    id_categoria.value = id
 }
--- js/produtos.js
+++ js/produtos.js
@@ -1,7 +1,48 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    var dropdown = document.getElementById("myDropdown")
-    dropdown.classList.add("show");
-  }
+function ListarProdutos(){
+  const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', 'http://loja.buiar.com/?key=3Tz81Yftd3C&c=produto&t=listar&f=json', false)
+    httpRequest.send()
+
+    resposta.dados.forEach( produto =>{
+
+      var divProduto = document.createElement('div')
+      divProduto.classList.add('categoria')
+      divProduto.setAttribute('id', produto.id)
+      
+      var p = document.createElement('p')
+      
+      p.innerHTML = categoria.id + '   ' + categoria.nome
+      
+      var divButtons = document.createElement('div')
+      divButtons.classList.add('buttons')
+
+      var link = document.createElement('a')
+      link.setAttribute('href','adm-alterar-categoria.html')
+
+      var iconEdit = document.createElement('img')
+      iconEdit.setAttribute('src','../assets/icon-edit.svg')
+      iconEdit.setAttribute('alt','Editar')
+      iconEdit.setAttribute('onclick','PreencherId('+ categoria.id +')')
+
+      var iconRemove = document.createElement('img')
+      iconRemove.setAttribute('src','../assets/icon-remove.svg')
+      iconRemove.setAttribute('alt','Remover')
+      iconRemove.setAttribute('onclick','RemoverCategoria('+ categoria.id +')')
+
+      link.appendChild(iconEdit)
+      divButtons.appendChild(link)
+      divButtons.appendChild(iconRemove)
+
+      divCategoria.appendChild(p)
+      divCategoria.appendChild(divButtons)
   
+      var divListarCategorias = document.getElementById('lista-categorias')
+      divListarCategorias.appendChild(divCategoria)
+    })
+}
