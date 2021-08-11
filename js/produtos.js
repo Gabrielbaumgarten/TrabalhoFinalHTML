adc6fe5b6660ad617ac103159a8c3e974a3ece32
Gabriel Baumgarten <gabrielbaumgarten@hotmail.com>
2021-08-10 23:05:38 -0300

    Tentativa de dropdown

---
+++ js/produtos.js
@@ -0,0 +1,7 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    var dropdown = document.getElementById("myDropdown")
+    dropdown.classList.add("show");
+  }
+  
