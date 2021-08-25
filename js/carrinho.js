99cf1ac971d5cc4a9c7f0495dc8db6c03711d42b
Gabriel Baumgarten <gabrielbaumgarten@hotmail.com>
2021-08-25 01:15:21 -0300

    Finalização da implementação do carrinho de compra

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,4 +1,4 @@
-function ListarCarrinho(){
+function AtualizarCarrinho(){
     const httpRequest = new XMLHttpRequest()
     var resposta = ''
 
@@ -53,4 +53,158 @@ function AlterarIconeCarrinho(numProdutos){
             imagem.src = '../assets/icon-carrinho-9+.svg'
             break
     }
+}
+
+function ListarCarrinho(){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', 'http://localhost:3000/carrinho', false)
+    httpRequest.send()
+
+    var id_itens_adicionados = []
+
+    console.log('carrinho:')
+    console.log(resposta)
+
+    resposta.forEach(item =>{
+        if(id_itens_adicionados.includes(item.produto_id)){
+            console.log('Item já adicionado')
+        } else{
+            id_itens_adicionados.push(item.produto_id)
+
+            var divProduto = document.createElement('div')
+            divProduto.classList.add('produto')
+
+            var imagem = new Image()
+            imagem.src = '../assets/' + item.imagem + '/img01.png'
+
+            var nome = document.createElement('h1')
+            nome.innerHTML = item.nome
+            
+            var divQuantidade = document.createElement('div')
+            divQuantidade.classList.add('quantidade')
+
+            var iconMinus = new Image()
+            iconMinus.src = '../assets/icon-minus.svg'
+            iconMinus.addEventListener('click',()=>{DiminuirItem(item.produto_id)})
+
+            var respostaQuantidade =''
+
+            httpRequest.onload = () => {
+                respostaQuantidade = JSON.parse(httpRequest.response)
+            }
+        
+            httpRequest.open('GET', 'http://localhost:3000/carrinho?produto_id='+item.produto_id, false)
+            httpRequest.send()
+
+
+            var quantidade = document.createElement('p')
+            quantidade.innerHTML = respostaQuantidade.length
+
+            // var produtoId = document.createElement('p')
+            // produtoId.innerHTML = item.produto_id
+            // produtoId.style.display = 'none'
+
+            var iconAdd = new Image()
+            iconAdd.src = '../assets/icon-add.svg'
+            iconAdd.addEventListener('click',()=>{AdicionarMaisItem(item.produto_id)})
+
+            var valor = document.createElement('p')
+            valor.innerHTML = 'R$ ' + (item.preco * quantidade.innerHTML) +',00'
+
+            var iconDelete = new Image()
+            iconDelete.src = '../assets/icon-remove.svg'
+            iconDelete.classList.add('icon-delete')
+            iconDelete.addEventListener('click',()=>{RemoverTodosItens(item.produto_id)})
+
+            divQuantidade.appendChild(iconMinus)
+            divQuantidade.appendChild(quantidade)
+            divQuantidade.appendChild(iconAdd)
+
+            divProduto.appendChild(imagem)
+            divProduto.appendChild(nome)
+            divProduto.appendChild(divQuantidade)
+            divProduto.appendChild(valor)
+            divProduto.appendChild(iconDelete)
+            // divProduto.appendChild(produtoId)
+
+            var produtoCarrinho = document.getElementById('ProdutosCarrinho')
+            produtoCarrinho.appendChild(divProduto)            
+
+        }
+    })
+}
+
+function SomarCarrinho(){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', 'http://localhost:3000/carrinho', false)
+    httpRequest.send()
+
+    var soma  = 0
+
+    resposta.forEach(item =>{
+        soma = soma + parseInt(item.preco)
+    })
+
+    document.getElementById('valor_conta').innerHTML = 'R$ ' + soma + ',00'
+}
+
+function AdicionarMaisItem(produto_id){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', 'http://localhost:3000/carrinho?produto_id='+produto_id, false)
+    httpRequest.send()
+
+    delete resposta[0].id
+    httpRequest.open('POST', 'http://localhost:3000/carrinho', false)
+    httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+    httpRequest.send(JSON.stringify(resposta[0]))
+}
+
+function DiminuirItem(produto_id){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', 'http://localhost:3000/carrinho?produto_id='+produto_id, false)
+    httpRequest.send()
+
+    httpRequest.open('DELETE', 'http://localhost:3000/carrinho/'+resposta[0].id, false)
+    httpRequest.send()
+}
+
+function RemoverTodosItens(produto_id){
+    const httpRequest = new XMLHttpRequest()
+    var resposta = ''
+
+    httpRequest.onload = () => {
+        resposta = JSON.parse(httpRequest.response)
+    }
+
+    httpRequest.open('GET', 'http://localhost:3000/carrinho?produto_id='+produto_id, false)
+    httpRequest.send()
+
+    resposta.forEach( item =>{
+        httpRequest.open('DELETE', 'http://localhost:3000/carrinho/'+item.id, false)
+        httpRequest.send()
+    })
 }
